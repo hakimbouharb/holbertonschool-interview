4671dcacc0fe3dca9863471284f168f324125bdc
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 <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>
2024-12-02 19:01:59 -0500

    binary tree node

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -1,25 +1,21 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - Creates a binary tree node.
- * @parent: Pointer to the parent node of the node to create.
- * @value: Value to put in the new node.
- *
- * Return: Pointer to the new node, or NULL on failure.
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: a pointer to the parent node
+ * @value: value to put in the next node
+ * Return: the address of the newly created node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *next;
 
-	if (new_node == NULL)
+	next = malloc(sizeof(binary_tree_t));
+	if (!next)
 		return (NULL);
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
-	return (new_node);
+	next->parent = parent;
+	next->n = value;
+	next->left = NULL;
+	next->right = NULL;
+	return (next);
 }
